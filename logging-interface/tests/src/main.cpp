dcf0be05fd20eaad473ebf7a065270c628835f08
sebasfalcone <sebastianfalcone01@gmail.com>
2023-03-20 22:30:29 +0000

    CL: - Clang-format changes

--- logging-interface/tests/src/main.cpp
+++ logging-interface/tests/src/main.cpp
@@ -1,12 +1,12 @@
 /**
  * @file main.cpp
  * @author Sebastian Falcone (sebastian.falcone@wazuh.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2023-02-26
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include <iostream>
