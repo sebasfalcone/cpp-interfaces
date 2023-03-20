996e0e7f3c5aa8c06b9a282a358569a85ac37d36
Sebastian Falcone <sebastianfalcone01@gmail.com>
2023-03-20 19:44:50 -0300

    CL: (#9)
    * CL:
    - Style changes over build workflow
    
    * CL:
    - Added cppcheck workflow
    
    * CL:
    - Added clang-format action
    
    * CL:
    - Clang-format changes

--- logging-interface/include/main.hpp
+++ logging-interface/include/main.hpp
@@ -1,15 +1,16 @@
 /**
  * @file main.hpp
  * @author Sebastian Falcone (sebastian.falcone@wazuh.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2023-02-26
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
-template <class T>
-T add(T a, T b){
+template<class T>
+T add(T a, T b)
+{
     return a + b;
 }
--- logging-interface/tests/src/test.hpp
+++ logging-interface/tests/src/test.hpp
@@ -1,10 +1,10 @@
 /**
  * @file test.hpp
  * @author Sebastian Falcone (sebastian.falcone@wazuh.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2023-02-26
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
