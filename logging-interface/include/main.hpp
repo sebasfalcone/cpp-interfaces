b24b74b847035846467f224bb6a547387df51a90
sebasfalcone <sebastian.falcone@wazuh.com>
2023-03-20 20:03:52 -0300

    CL: - Updated files header info

--- logging-interface/include/main.hpp
+++ logging-interface/include/main.hpp
@@ -1,6 +1,6 @@
 /**
  * @file main.hpp
- * @author Sebastian Falcone (sebastian.falcone@wazuh.com)
+ * @author Sebastian Falcone (sebastianfalcone01@gmail.com)
  * @brief 
  * @version 1.0
  * @date 2023-02-26
--- logging-interface/tests/src/test.hpp
+++ logging-interface/tests/src/test.hpp
@@ -1,6 +1,6 @@
 /**
  * @file test.hpp
- * @author Sebastian Falcone (sebastian.falcone@wazuh.com)
+ * @author Sebastian Falcone (sebastianfalcone01@gmail.com)
  * @brief 
  * @version 1.0
  * @date 2023-02-26
